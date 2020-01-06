dbd4f52364b36800e93a4d323cd02dc15fbf1cb2
Murtuza Zabuawala <murtuza.zabuawala@enterprisedb.com>
2020-01-06 13:44:37 +0530

    Update copyright notices for 2020.

--- sslutils.c
+++ sslutils.c
@@ -2,7 +2,7 @@
  *
  * Postgres Enterprise Manager - Openssl utils plugin
  *
- * Copyright (C) 2011 - 2019, EnterpriseDB Corporation. All rights reserved.
+ * Copyright (C) 2011 - 2020, EnterpriseDB Corporation. All rights reserved.
  *
  * This code is licensed under the EnterpriseDB Limited Use License for use
  * by customers with an active subscription to EDB Postgres Standard,
