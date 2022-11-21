7b56762b56619e6fee4b1dab03a8c8bf5b8cb5e7
Ashesh Vashi <ashesh.vashi@enterprisedb.com>
2022-11-21 15:57:34 +0530

    chore: Delete reference of linux/linux-x64/solaris/solaris-x84 from the build script.

--- installer/sslutils/build.sh
+++ installer/sslutils/build.sh
@@ -3,24 +3,6 @@
 SSLUTILS_CACHE_DIR=$WD/sslutils/staging
 # Read the various build scripts
 
-# Mac OS X
-if [ $PG_ARCH_OSX = 1 ];
-then
-    source $WD/sslutils/build-osx.sh
-fi
-
-# Linux
-if [ $PG_ARCH_LINUX = 1 ];
-then
-    source $WD/sslutils/build-linux.sh
-fi
-
-# Linux x64
-if [ $PG_ARCH_LINUX_X64 = 1 ];
-then
-    source $WD/sslutils/build-linux-x64.sh
-fi
-
 # Windows
 if [ $PG_ARCH_WINDOWS = 1 ];
 then
@@ -31,18 +13,6 @@ fi
 if [ $PG_ARCH_WINDOWS_X64 = 1 -a $PG_MAJOR_VERSION != "8.4" ];
 then
     source $WD/sslutils/build-windows-x64.sh
-fi
-
-# Solaris x64
-if [ $PG_ARCH_SOLARIS_X64 = 1 -a $PG_MAJOR_VERSION != "8.4" ];
-then
-    source $WD/sslutils/build-solaris-x64.sh
-fi
-
-# Solaris sparc
-if [ $PG_ARCH_SOLARIS_SPARC = 1 -a $PG_MAJOR_VERSION != "8.4" ];
-then
-    source $WD/sslutils/build-solaris-sparc.sh
 fi
 
 ################################################################################
@@ -66,26 +36,7 @@ _prep_sslutils() {
     cd $WD/pvt_packages/PEM
     git pull
 
-    cp -r $WD/pvt_packages/PEM/plugins/sslutils $WD/sslutils/source/sslutils
-
-    # Mac OS X
-    if [ $PG_ARCH_OSX = 1 ];
-    then
-        #_prep_sslutils_osx 
-        echo "Not yet supported (ssutils -  Mac OS X )"
-    fi
-
-    # Linux
-    if [ $PG_ARCH_LINUX = 1 ];
-    then
-        _prep_sslutils_linux 
-    fi
-
-    # Linux x64
-    if [ $PG_ARCH_LINUX_X64 = 1 ];
-    then
-        _prep_sslutils_linux_x64 
-    fi
+    cp -r $WD/pvt_packages/sslutils $WD/sslutils/source/sslutils
 
     # Windows
     if [ $PG_ARCH_WINDOWS = 1 ];
@@ -99,18 +50,6 @@ _prep_sslutils() {
         _prep_sslutils_windows_x64 
     fi
 
-    # Solaris x64
-    if [ $PG_ARCH_SOLARIS_X64 = 1 -a $PG_MAJOR_VERSION != "8.4" ]; then
-        #_prep_sslutils_solaris_x64 
-        echo "Not yet supported (ssutils - Solaris x64)"
-    fi
-
-    # Solaris sparc
-    if [ $PG_ARCH_SOLARIS_SPARC = 1 -a $PG_MAJOR_VERSION != "8.4" ]; then
-        #_prep_sslutils_solaris_sparc 
-        echo "Not yet supported (ssutils - Solaris sparc)"
-    fi
-
 }
 
 ################################################################################
@@ -118,25 +57,6 @@ _prep_sslutils() {
 ################################################################################
 
 _build_sslutils() {
-
-    # Mac OSX
-    if [ $PG_ARCH_OSX = 1 ];
-    then
-        #_build_sslutils_osx 
-        echo "Not yet supported (sslutils - Mac OSX)"
-    fi
-
-    # Linux
-    if [ $PG_ARCH_LINUX = 1 ];
-    then
-        _build_sslutils_linux 
-    fi
-
-    # Linux x64
-    if [ $PG_ARCH_LINUX_X64 = 1 ];
-    then
-        _build_sslutils_linux_x64 
-    fi
 
     # Windows
     if [ $PG_ARCH_WINDOWS = 1 ];
@@ -148,18 +68,6 @@ _build_sslutils() {
     if [ $PG_ARCH_WINDOWS_X64 = 1 -a $PG_MAJOR_VERSION != "8.4" ];
     then
         _build_sslutils_windows_x64 
-    fi
-
-    # Solaris x64
-    if [ $PG_ARCH_SOLARIS_X64 = 1 -a $PG_MAJOR_VERSION != "8.4" ]; then
-        #_build_sslutils_solaris_x64 
-        echo "Not yet supported (ssutils - Solaris x64)"
-    fi
-
-    # Solaris sparc
-    if [ $PG_ARCH_SOLARIS_SPARC = 1 -a $PG_MAJOR_VERSION != "8.4" ]; then
-        #_build_sslutils_solaris_sparc 
-        echo "Not yet supported (ssutils - Solaris sparc)"
     fi
 
 }
@@ -174,25 +82,6 @@ _postprocess_sslutils() {
 
     cd $WD/sslutils
 
-    # Mac OSX
-    if [ $PG_ARCH_OSX = 1 ];
-    then
-        #_postprocess_sslutils_osx 
-        echo "Not yet supported (sslutils - Mac OSX)"
-    fi
-
-    # Linux
-    if [ $PG_ARCH_LINUX = 1 ];
-    then
-        _postprocess_sslutils_linux 
-    fi
-
-    # Linux x64
-    if [ $PG_ARCH_LINUX_X64 = 1 ];
-    then
-        _postprocess_sslutils_linux_x64 
-    fi
-
     # Windows
     if [ $PG_ARCH_WINDOWS = 1 ];
     then
@@ -203,18 +92,6 @@ _postprocess_sslutils() {
     if [ $PG_ARCH_WINDOWS_X64 = 1 -a $PG_MAJOR_VERSION != "8.4" ];
     then
         _postprocess_sslutils_windows_x64 
-    fi
-
-    # Solaris x64
-    if [ $PG_ARCH_SOLARIS_X64 = 1 -a $PG_MAJOR_VERSION != "8.4" ]; then
-        #_postprocess_sslutils_solaris_x64 
-        echo "Not yet supported (ssutils - Solaris x64)"
-    fi
-
-    # Solaris sparc
-    if [ $PG_ARCH_SOLARIS_SPARC = 1 -a $PG_MAJOR_VERSION != "8.4" ]; then
-        #_postprocess_sslutils_solaris_sparc 
-        echo "Not yet supported (ssutils - Solaris sparc)"
     fi
 
     rm -f ${SSLUTILS_CACHE_DIR}/sslutils-$PG_MAJOR_VERSION.tar.gz
