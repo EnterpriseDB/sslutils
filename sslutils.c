ea1a9f9d25499ce3bd0318195b68c573707e0ee6
edbjunwang <junwang@enterprisedb.com>
2026-05-25 16:16:58 +0800

    fix(PEM-6043): Out-of-Bounds Read in ASN1_GetTimeT — Malformed Certificate Causes Memory Corruption

--- sslutils.c
+++ sslutils.c
@@ -1691,44 +1691,19 @@ out:
 
 /*
 * Convert ASN1_TIME to tm.
+*  -- Using the OpenSSL (like ASN1_TIME_to_tm which is vailable since OpenSSL 1.1.0)
 *
 */
 time_t
 ASN1_GetTimeT(const ASN1_TIME* time)
 {
 	struct tm t;
-	const char* str = (const char*)time->data;
-	size_t i = 0;
-
 	memset(&t, 0, sizeof(t));
 
-	if (time->type == V_ASN1_UTCTIME) {/* two digit year */
-		t.tm_year = (str[i++] - '0') * 10;
-		t.tm_year += (str[i++] - '0');
-		if (t.tm_year < 70)
-			t.tm_year += 100;
-	}
-	else if (time->type == V_ASN1_GENERALIZEDTIME) {/* four digit year */
-		t.tm_year = (str[i++] - '0') * 1000;
-		t.tm_year += (str[i++] - '0') * 100;
-		t.tm_year += (str[i++] - '0') * 10;
-		t.tm_year += (str[i++] - '0');
-		t.tm_year -= 1900;
-	}
+	if (!ASN1_TIME_to_tm(time, &t))
+		return (time_t)-1;   // error: malformed time field
 
-	t.tm_mon = (str[i++] - '0') * 10;
-	t.tm_mon += (str[i++] - '0') - 1; // -1 since January is 0 not 1.
-	t.tm_mday = (str[i++] - '0') * 10;
-	t.tm_mday += (str[i++] - '0');
-	t.tm_hour = (str[i++] - '0') * 10;
-	t.tm_hour += (str[i++] - '0');
-	t.tm_min = (str[i++] - '0') * 10;
-	t.tm_min += (str[i++] - '0');
-	t.tm_sec = (str[i++] - '0') * 10;
-	t.tm_sec += (str[i++] - '0');
-
-	/* Note: we did not adjust the time based on time zone information */
-	return mktime(&t);
+	return timegm(&t);
 }
 
 /*
